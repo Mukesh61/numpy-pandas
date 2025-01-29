0a5b23188217ceae5a0aac50ed96273506789c13
Mukesh61 <45516247+Mukesh61@users.noreply.github.com>
2025-01-29 23:05:08 +0530

    Create 02_pandas_series.py

---
+++ 02_pandas_series.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+data = pd.Series(np.random.randint(10,10,2000))
+'''
+what is vectorization? it is also know as array programming. instead of process data one by one in loop, vectorization perform at once. like in pandas and numpy.
+'''
+
