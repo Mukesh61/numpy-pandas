7ac60161a35b962371696c7bd692cb0fb100485b
Mukesh61 <45516247+Mukesh61@users.noreply.github.com>
2025-02-26 18:18:34 +0530

    Update 01_create_numpy_array.py

--- 01_create_numpy_array.py
+++ 01_create_numpy_array.py
@@ -25,3 +25,21 @@ print(numpy_array.ndim) # 2
 print(numpy_array.shape) # (2, 6)
 print(numpy_array.size) # 12
 print(numpy_array.dtype) # int64
+
+np.ones(5,) #array([1., 1., 1., 1., 1.])
+
+np.zeros((3,5), dtype=int)
+'''
+array([[0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0]])
+'''
+
+np.arange(15) #array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14])
+
+np.linspace(0, 200, 5) #array([  0.,  50., 100., 150., 200.])
+
+numpy_array.reshape(1, 12) #array([[ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12]])
+
+
+
