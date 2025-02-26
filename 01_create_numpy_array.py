0c28338f36bb4e19d2010bf3a4ec75fa032ffbec
Mukesh61 <45516247+Mukesh61@users.noreply.github.com>
2025-02-27 01:07:40 +0530

    Update 01_numpy_array_revision.py

--- 01_create_numpy_array.py
+++ 01_create_numpy_array.py
@@ -41,5 +41,38 @@ np.linspace(0, 200, 5) #array([  0.,  50., 100., 150., 200.])
 
 numpy_array.reshape(1, 12) #array([[ 1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12]])
 
+# Indexing & slicing arrays ie. numpy_array[start:stop:step_size], in 2D array numpy_array[start:stop:step_size,start:stop:step_size]
+numpy_array[0]  #first element
+numpy_array[-1] #last element
+
+# Arithmatic operation
+numpy_array + 2
+numpy_array * 3
+
+#Filter condition
+numpy_array[(other_array == 454) | (other_array < 80)]  # or condition
+numpy_array[(other_array == 454) & (other_array < 80)]  # and condition
+
+#where like case statement in sql
+np.where(logical_test_condition, this value if true, otherwise this value if false)
+
+np.where(numpy_array <0, "negative number", "positive number")
+
+# Aggregation 
+numpy_array.sum()
+numpy_array.max()
+numpy_array.mean()
+numpy_array.min()
+
+numpy_array.sum(axis=0) #aggregates across rows, like add row 0 of column 0 + row 0 of column 1 like this
+numpy_array.sum(axis=1) #aggregates across column, like add all column value in a row. 
+
+#Array functions
+np.median(numpy_array)
+np.percentile(numpy_array)
+np.unique(numpy_array)
+np.sqrt(numpy_array)
+
+
 
 
